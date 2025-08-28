3adc3a087eeb18648074acd3834ce868f1dd7142
muhammadoby <07muhammadoby@gmail.com>
2025-08-28 16:30:09 +0800

    bugfix: error in production

--- src/components/TextWithImage.jsx
+++ src/components/TextWithImage.jsx
@@ -1,27 +1,27 @@
-import React from "react";
-import HandImage from "../assets/image13.png"
-
 export default function TextWithImage() {
   return (
-    <section class="text-img bg-linear-to-t from-[#ffffff] to-[#99B9EB] px-4 py-10 md:px-32 md:py-32 relative">
-      <div className="flex flex-col md:flex-row justify-center items-center gap-18">
-          {/* <!-- Text --> */}
-          <div class="hero-text w-full">
-            <h1 className="text-[#012D70] text-[34px] md:text-[48px] text-center md:text-left pb-4 md:pb-0">
-              An integrated digital solution with
-              <span class="text-[#F05A29]"> guaranteed security and scalability</span>
-            </h1>
-            <p className="text-[18px] md:text-[20px] text-[#898895]">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vitae
-              mauris id eros interdum sagittis a ut eros. Nunc scelerisque imperdiet
-              ex pretium consequat. Vivamus viverra fermentum aliquam. Aliquam
-              tristique, orci vel vulputate vehicula, erat libero convallis quam,
-              vitae laoreet dolor nisi nec elit.
-            </p>
-          </div>
-          <div className="hand-image w-full z-9">
-            <img src={HandImage} alt="HandImage" />
-          </div>
+    <section className="text-img bg-linear-to-t from-[#ffffff] to-[#99B9EB] px-4 py-10 md:px-32 md:py-32 relative">
+      <div classname="flex flex-col md:flex-row justify-center items-center gap-18">
+        {"{"}/* {/* Text */} */{"}"}
+        <div className="hero-text w-full">
+          <h1 classname="text-[#012D70] text-[34px] md:text-[48px] text-center md:text-left pb-4 md:pb-0">
+            An integrated digital solution with
+            <span className="text-[#F05A29]">
+              {" "}
+              guaranteed security and scalability
+            </span>
+          </h1>
+          <p classname="text-[18px] md:text-[20px] text-[#898895]">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+            vitae mauris id eros interdum sagittis a ut eros. Nunc scelerisque
+            imperdiet ex pretium consequat. Vivamus viverra fermentum aliquam.
+            Aliquam tristique, orci vel vulputate vehicula, erat libero
+            convallis quam, vitae laoreet dolor nisi nec elit.
+          </p>
+        </div>
+        <div classname="hand-image w-full z-9">
+          <img src="{HandImage}" alt="HandImage" />
+        </div>
       </div>
     </section>
   );
