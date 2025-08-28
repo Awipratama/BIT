4d7dd72fe5cea8c6b616157202973b1fa29b85f5
muhammadoby <07muhammadoby@gmail.com>
2025-08-28 16:46:36 +0800

    bugfix: missing image in textWithImage Components

--- src/components/TextWithImage.jsx
+++ src/components/TextWithImage.jsx
@@ -6,10 +6,10 @@ export default function TextWithImage() {
     <section className="text-img bg-linear-to-t from-[#ffffff] to-[#99B9EB] px-4 py-10 md:px-32 md:py-32 relative">
       <div className="flex flex-col md:flex-row justify-center items-center gap-18">
           {/* <!-- Text --> */}
-          <div class="hero-text w-full">
+          <div className="hero-text w-full">
             <h1 className="text-[#012D70] text-[34px] md:text-[48px] text-center md:text-left pb-4 md:pb-0">
               An integrated digital solution with
-              <span class="text-[#F05A29]"> guaranteed security and scalability</span>
+              <span className="text-[#F05A29]"> guaranteed security and scalability</span>
             </h1>
             <p className="text-[18px] md:text-[20px] text-[#898895]">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vitae
