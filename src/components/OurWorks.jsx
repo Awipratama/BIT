898f32a2563b9d9ceca8eef4d7328e7567d1c115
ardi <awipratama05122006@gmail.com>
2025-08-28 16:59:08 +0800

    work link

--- src/components/OurWorks.jsx
+++ src/components/OurWorks.jsx
@@ -14,18 +14,18 @@ import img11 from "../assets/image11.png";
 import img12 from "../assets/image12.png";
 
 const works = [
-  { name: "BikinCV.com", img: img1 },
-  { name: "LokerBali.info", img: img2 },
-  { name: "LokerJakarta.id", img: img3 },
-  { name: "Cari-Kos.com", img: img4 },
-  { name: "Resepedia.com", img: img5 },
-  { name: "Kooliner.com", img: img6 },
-  { name: "AWBaliDigital.com", img: img7 },
-  { name: "LowonganHotelBali.com", img: img8 },
-  { name: "Weddingku.id", img: img9 },
-  { name: "Cvresume.co", img: img10 },
-  { name: "Jktliving.com", img: img11 },
-  { name: "Balifast.com", img: img12 },
+  { name: "BikinCV.com", img: img1, link: "https://www.bikincv.com/" },
+  { name: "LokerBali.info", img: img2, link: "https://www.lokerbali.info/" },
+  { name: "LokerJakarta.id", img: img3, link: "https://www.lokerjakarta.id/" },
+  { name: "Cari-Kos.com", img: img4, link: "https://www.cari-kos.com/" },
+  { name: "Reseppedia.com", img: img5, link: "https://www.reseppedia.com/" },
+  { name: "Kooliner.com", img: img6, link: "https://www.kooliner.com/" },
+  { name: "AWBaliDigital.com", img: img7, link: "https://www.awbalidigital.com/" },
+  { name: "LowonganHotelBali.com", img: img8, link: "https://www.lowonganhotelbali.com/" },
+  { name: "Weddingku.id", img: img9, link: "https://www.weddingku.id/" },
+  { name: "Cvresume.co", img: img10, link: "https://www.cvresume.co/" },
+  { name: "Jktliving.com", img: img11, link: "https://www.jktliving.com/" },
+  { name: "Balifast.com", img: img12, link: "https://www.balifast.com/" },
 ];
 
 export default function OurWorks() {
@@ -40,17 +40,23 @@ export default function OurWorks() {
         {/* Grid */}
         <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-8">
           {works.map((work, index) => (
-            <div key={index} className="bg-[#00000000]">
-              <div className="bg-white rounded-lg shadow-md flex flex-col items-center p-6 transition transform hover:scale-105 cursor-pointer">
-                {/* Logo (petunjuk: taruh logo kamu di /public/images/ dan sesuaikan src) */}
-                <img
-                  src={work.img}
-                  alt={work.name}
-                  className="max-h-50 object-contain mb-4"
-                />
+            <a href={work.link}
+            key={index}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="bg-[#00000000]">
+              <div key={index} className="bg-[#00000000]">
+                <div className="bg-white rounded-lg shadow-md flex flex-col items-center p-6 transition transform hover:scale-105 cursor-pointer">
+                  {/* Logo (petunjuk: taruh logo kamu di /public/images/ dan sesuaikan src) */}
+                  <img
+                    src={work.img}
+                    alt={work.name}
+                    className="max-h-50 object-contain mb-4"
+                  />
+                </div>
+                <p className="text-white text-center font-regular text-sm md:text-xl pt-2 hover:underline cursor-pointer">{work.name}</p>
               </div>
-              <p className="text-white text-center font-regular text-sm md:text-xl pt-2 hover:underline cursor-pointer">{work.name}</p>
-            </div>
+            </a>
           ))}
         </div>
       </div>
